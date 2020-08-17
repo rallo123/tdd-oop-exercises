d35695a686ed39a0385819447fc413a757833bef
Rasmus <ralloskytten@gmail.com>
2020-08-17 15:51:28 +0200

    Opgaveløsning

--- Lecture 2/Lecture 2 Exercises/Person.cs
+++ Lecture 2/Lecture 2 Exercises/Person.cs
@@ -2,13 +2,44 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Lecture_1
 {
     public class Person
     {
-        string FirstName;
-        string LastName;
-        int Age;
+        string _firstName;
+        string _lastName;
+        int _age;
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                if (Regex.IsMatch(value, @"^[a-zA-Z]+$") && value.Length < 100) _firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get {return _lastName;}
+            set
+            {
+                if (Regex.IsMatch(value, @"^[a-zA-Z]+$") && value.Length < 100) _lastName = value; 
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                if (value > 0) _age = value;
+            }
+        }
     }
 }
