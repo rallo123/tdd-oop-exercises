baff7af57a9995e92b854e548648f1d657ac092b
Ole van Santen <40497940+OleVanSanten@users.noreply.github.com>
2020-08-25 16:17:53 +0200

    Bug fix

--- TestTools/TestTools/Structure/Generic/ConstructorElement.cs
+++ TestTools/TestTools/Structure/Generic/ConstructorElement.cs
@@ -13,33 +13,43 @@ namespace TestTools.Structure.Generic
         public TRoot Invoke(TRoot instance) => Invoke();
     }
 
-    public class ConstructorElement<TRoot, T1> : ConstructorElement, IFunc<T1, TRoot>
+    public class ConstructorElement<TRoot, T1> : ConstructorElement, IFunc<TRoot, T1, TRoot>
     {
         public ConstructorElement(ConstructorInfo constructorInfo) : base(constructorInfo) { }
+
         public TRoot Invoke(T1 par1) => (TRoot)base.Invoke(par1);
+        public TRoot Invoke(TRoot instance, T1 par1) => (TRoot)base.Invoke(instance, par1);
     }
 
-    public class ConstructorElement<TRoot, T1, T2> : ConstructorElement, IFunc<T1, T2, TRoot>
+    public class ConstructorElement<TRoot, T1, T2> : ConstructorElement, IFunc<TRoot, T1, T2, TRoot>
     {
         public ConstructorElement(ConstructorInfo constructorInfo) : base(constructorInfo) { }
+
         public TRoot Invoke(T1 par1, T2 par2) => (TRoot)base.Invoke(par1, par2);
+        public TRoot Invoke(TRoot instance, T1 par1, T2 par2) => (TRoot)base.Invoke(instance, par1, par2);
     }
 
-    public class ConstructorElement<TRoot, T1, T2, T3> : ConstructorElement, IFunc<T1, T2, T3, TRoot>
+    public class ConstructorElement<TRoot, T1, T2, T3> : ConstructorElement, IFunc<TRoot, T1, T2, T3, TRoot>
     {
         public ConstructorElement(ConstructorInfo constructorInfo) : base(constructorInfo) { }
+
         public TRoot Invoke(T1 par1, T2 par2, T3 par3) => (TRoot)base.Invoke(par1, par2, par3);
+        public TRoot Invoke(TRoot instance, T1 par1, T2 par2, T3 par3) => (TRoot)base.Invoke(instance, par1, par2, par3);
     }
 
-    public class ConstructorElement<TRoot, T1, T2, T3, T4> : ConstructorElement, IFunc<T1, T2, T3, T4, TRoot>
+    public class ConstructorElement<TRoot, T1, T2, T3, T4> : ConstructorElement, IFunc<TRoot, T1, T2, T3, T4, TRoot>
     {
         public ConstructorElement(ConstructorInfo constructorInfo) : base(constructorInfo) { }
+        
         public TRoot Invoke(T1 par1, T2 par2, T3 par3, T4 par4) => (TRoot)base.Invoke(par1, par2, par3, par4);
+        public TRoot Invoke(TRoot instance, T1 par1, T2 par2, T3 par3, T4 par4) => (TRoot)base.Invoke(instance, par1, par2, par3, par4);
     }
 
-    public class ConstructorElement<TRoot, T1, T2, T3, T4, T5> : ConstructorElement, IFunc<T1, T2, T3, T4, T5, TRoot>
+    public class ConstructorElement<TRoot, T1, T2, T3, T4, T5> : ConstructorElement, IFunc<TRoot, T1, T2, T3, T4, T5, TRoot>
     {
         public ConstructorElement(ConstructorInfo constructorInfo) : base(constructorInfo) { }
+
         public TRoot Invoke(T1 par1, T2 par2, T3 par3, T4 par4, T5 par5) => (TRoot)base.Invoke(par1, par2, par3, par4, par5);
+        public TRoot Invoke(TRoot instance, T1 par1, T2 par2, T3 par3, T4 par4, T5 par5) => (TRoot)base.Invoke(instance, par1, par2, par3, par4, par5);
     }
 }
