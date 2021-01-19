57570a1edd2db398652b5d0d769326ddb2b06aad
Ole van Santen <dxiedx@protonmail.com>
2021-01-19 11:00:06 +0100

    Adding UnitTestFileSystem to make filesystem tests.

---
+++ TestTools/TestTools/Integrated/UnitTestFileSystem.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Abstractions;
+using System.Text;
+
+namespace TestTools.Integrated
+{
+    // based on IFileSystem
+    public class UnitTestFileSystem
+    {
+        public void Act(Action<IFileSystem> action)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AssertObject FileAssert { get; }
+
+        public class AssertObject
+        {
+            // State of file systems
+            public void FileExists(string path)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void FileDoesNotExist(string path)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void DirectoryExist(string path)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void DirectoryDoesNotExist(string path)
+            {
+                throw new NotImplementedException();
+            }
+
+            // Mutatation in file systems
+            public void FileCreated(string path)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void FileDeleted(string path)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Created(string path)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Deleted(string path)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
