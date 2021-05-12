31c6a83944b823dcdc2161e9c077c9f3deda41bc
Ole van Santen <dxiedx@protonmail.com>
2021-05-12 23:05:47 +0200

    Fixing bug which occured when PropertyType also needed to translated, like in Person.Mother in Lecture 2.

--- TestTools/TestTools/Structure/MemberVerifiers/UnchangedPropertyTypeVerifier.cs
+++ TestTools/TestTools/Structure/MemberVerifiers/UnchangedPropertyTypeVerifier.cs
@@ -12,13 +12,14 @@ namespace TestTools.Structure
         public override void Verify(MemberInfo originalMember, MemberInfo translatedMember)
         {
             PropertyInfo originalProperty = originalMember as PropertyInfo;
+            Type originalPropertyType = Service.TranslateType(originalProperty.PropertyType);
 
             Verifier.VerifyMemberType(translatedMember, new MemberTypes[] { MemberTypes.Field, MemberTypes.Property });
 
             if (translatedMember is FieldInfo translatedField)
-                Verifier.VerifyFieldType(translatedField, originalProperty.PropertyType);
+                Verifier.VerifyFieldType(translatedField, originalPropertyType);
             else if (translatedMember is PropertyInfo translatedProperty)
-                Verifier.VerifyPropertyType(translatedProperty, originalProperty.PropertyType);
+                Verifier.VerifyPropertyType(translatedProperty, originalPropertyType);
             else throw new NotImplementedException();
         }
     }
